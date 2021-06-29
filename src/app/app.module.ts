d1aaae43a9fcf6b6683087c797d5fced82084f54
Aisak <santaellaisaac2@gmail.com>
2021-06-28 20:10:42 -0400

    template incorporado al 60%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { FirstComponent } from './body/first/first.component';
+import { AboutComponent } from './body/about/about.component';
+import { ResumeComponent } from './body/resume/resume.component';
+import { PortfolioComponent } from './body/portfolio/portfolio.component';
+import { ContactComponent } from './body/contact/contact.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { BodyComponent } from './body/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FirstComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
