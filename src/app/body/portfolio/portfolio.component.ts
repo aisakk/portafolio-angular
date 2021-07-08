6ea489ca5e8031c68811caf54b4d5a1ae34ccc10
Aisak <santaellaisaac2@gmail.com>
2021-07-08 01:34:35 -0400

    Listo a produccion aunque faltan mas imagenes

--- src/app/body/portfolio/portfolio.component.ts
+++ src/app/body/portfolio/portfolio.component.ts
@@ -24,7 +24,19 @@ export class PortfolioComponent implements OnInit {
       banner: 'assets/img/oasis.b7a10bdf.png',
       pdfOrLink: 'assets/img/pdfportfolio/Pantallas.23c4b88f.pdf',
       type: 'filter-app',
-    }
+    },
+    {
+      banner: 'assets/img/ideasbox.png',
+      pdfOrLink: 'assets/img/pdfportfolio/ideasbox.pdf',
+      text: 'ideasbox',
+      type: 'filter-web',
+    },
+    {
+      banner: 'assets/img/figma.png',
+      pdfOrLink: 'https://www.figma.com/file/Z1ud2u9O4WygrR73o8Tdku/Portfolio?node-id=0%3A1',
+      text: 'Figma',
+      type: 'filter-web',
+    },
   ]
 
   constructor() { }
