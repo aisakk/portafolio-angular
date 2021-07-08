45445563093aeb7860915fa646626f02d1d1c0c1
Aisak <santaellaisaac2@gmail.com>
2021-07-08 01:27:00 -0400

    Portafolio listo falta subir mas imagenes

--- src/app/body/contact/contact.component.ts
+++ src/app/body/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Type } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-contact',
@@ -12,19 +13,64 @@ export class ContactComponent implements OnInit {
   email: string = '';
   title: string = '';
   message: string = '';
-  constructor(private http:HttpClient) { }
+  load: boolean = false;
+  success: boolean = false;
+  error: boolean = false;
+  send:boolean = true;
+
+  constructor(private http:HttpClient) { 
+  }
 
   ngOnInit(): void {
   }
+  
 
   sendMessage(){
-    let url = 'localhost:8080/contact.php'
-    let formData = new FormData();
-    formData.append('name', this.name)
-    formData.append('email', this.email)
-    formData.append('title', this.title)
-    formData.append('message', this.message)
-    this.http.post(url, formData).toPromise().then((data:any) =>console.log(data))
+    let url = 'http://localhost/portfolio/contacto.php'
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json; charset=utf-8');
+    let json = {
+      name: this.name,
+      email: this.email,
+      title: this.title,
+      message: this.message
+    }
+    this.load = true
+    this.http.post(url, JSON.stringify(json), {headers}).subscribe(
+      (data) => {
+        console.log(data);
+     },
+    response => {
+        if(response.status < 300 || response.status >= 200){
+       
+          setTimeout(()=>{
+            this.load = false
+            this.success = true 
+          }, 3000);
+          setTimeout(()=>{
+            this.success = false
+          }, 4000)
+        
+        }else{
+          this.load = true
+          setTimeout(()=>{
+            this.error = true
+          }, 3000)
+          setTimeout(()=>{
+            this.error = false
+
+          }, 4000)
+          console.log(response)
+        }
+    })
+  }
+
+  sendDisabled(){
+    if(this.name != '' && this.email != '' && this.title != '' && this.message != ''){
+     return this.send = false
+    }else{
+      return this.send = true
+    }
   }
 
 }
--- src/app/body/portfolio/portfolio.component.ts
+++ src/app/body/portfolio/portfolio.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  Images = [
+    {
+      banner: 'assets/img/divisas.129e8e61.jpg',
+      pdfOrLink: 'https://aisakk.github.io/ConvertDivisa/',
+      type: 'filter-web',
+      text: 'ConvertDivisa'
+    },
+    {
+      banner: 'assets/img/petroleo.4da21717.jpeg',
+      pdfOrLink: 'assets/img/pdfportfolio/Inteve.41fcb78d.pdf',
+      type: 'filter-web',
+      text: 'SIGESDOC'
+    },
+    {
+      banner: 'assets/img/oasis.b7a10bdf.png',
+      pdfOrLink: 'assets/img/pdfportfolio/Pantallas.23c4b88f.pdf',
+      type: 'filter-app',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
