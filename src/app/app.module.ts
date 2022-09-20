e2710b09f421bd306d6f2cab5f1e9396db7a9261
Aisak <santaellaisaac2@gmail.com>
2022-09-20 15:45:07 -0400

    listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 import { AboutComponent } from './body/about/about.component';
 import { ResumeComponent } from './body/resume/resume.component';
 import { PortfolioComponent } from './body/portfolio/portfolio.component';
-import { ContactComponent } from './body/contact/contact.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { BodyComponent } from './body/body/body.component';
 import { SkillsComponent } from './body/skills/skills.component';
@@ -22,7 +21,6 @@ import { SkillsComponent } from './body/skills/skills.component';
     AboutComponent,
     ResumeComponent,
     PortfolioComponent,
-    ContactComponent,
     FooterComponent,
     BodyComponent,
     SkillsComponent,
